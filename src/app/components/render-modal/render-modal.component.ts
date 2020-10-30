15d4bce0a80cae79e0e2dbe79c035bf444aecf54
Cesar Romero Arroyo <cesar.romero@shopperkit.com>
2020-10-29 20:43:10 -0500

    ready behavior

--- src/app/components/render-modal/render-modal.component.ts
+++ src/app/components/render-modal/render-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Widget } from 'src/app/core/models/widget.model';
+import { ReactiveService } from 'src/app/core/services/reactive.service';
+import { WidgetTypeEnum } from '../../core/enums/widget-type.enum';
 
 @Component({
   selector: 'app-render-modal',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./render-modal.component.scss']
 })
 export class RenderModalComponent implements OnInit {
-
-  constructor() { }
+  show = false;
+  components: Widget[] = [];
+  constructor(
+    private reactive: ReactiveService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
+    this.reactive.getObservable().subscribe((data) => {
+      this.show = data['renderModal'];
+      if(data['widgets']) {
+        this.components = data['widgets'];
+      }
+    });
+  }
+
+
+  closeModal(){
+    this.reactive.setData({renderModal: false});
   }
 
 }
